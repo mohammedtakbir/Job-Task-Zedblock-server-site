da54f86dd4855758b5a8ae648c9bb192c29946a9
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-01 09:05:39 +0600

    register and login api is created

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //* middleware
 app.use(cors());
@@ -12,7 +14,42 @@ app.get('/', (req, res) => {
 })
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.drjbcpx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run() {
+    try {
+        const usersCollection = client.db('zedblock').collection('users')
+
+        //* register
+        app.post('/register', async (req, res) => {
+            const user = req.body;
+            const isAlreadyRegister = await usersCollection.findOne(user);
+            const isRegister = isAlreadyRegister?.name === user?.name;
+            if (isRegister) {
+                return res.send({ message: 'The user name already exist' })
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send({ user: user });
+        })
+
+        //* login
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const isUserExist = await usersCollection.findOne(user);
+            if (!isUserExist) {
+                return res.send({ message: 'User not found' })
+            }
+            res.send({ user: user });
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.log(err))
 
 
 
